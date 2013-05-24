f254570eb16ec152379a53705d86d1affad16600
Daniel Keller <dbk40@case.edu>
2013-05-24 15:44:55 +0000

    handling for no input files

--- Lispg.hs
+++ Lispg.hs
@@ -20,17 +20,17 @@ import qualified Data.Map as Map
 
 main = do
     args <- getArgs
-    let fName = case args of
-            n:_ -> n
-            otherwise -> error "not enough argumnets"
-    code <- parseFile fName >>= return.deSugar
-    --putStrLn $ printFile code
-    flip mapM_ code (\ (Binding v e) ->
-        inferT builtinTy e)
-    --putStrLn $ intercalate "\n\n" $ map show $ fileToCps code
-    --putStrLn $ printCpsFile $ fileToCps code
-    --codeGen "test.bc" $ fileToCps code
-    --evExpr code
+    case args of
+        [] -> evExpr []
+        fName:_ -> do
+            code <- parseFile fName >>= return.deSugar
+            --putStrLn $ printFile code
+            flip mapM_ code (\ (Binding v e) ->
+                inferT builtinTy e)
+            --putStrLn $ intercalate "\n\n" $ map show $ fileToCps code
+            --putStrLn $ printCpsFile $ fileToCps code
+            --codeGen "test.bc" $ fileToCps code
+            evExpr code
 
 evExpr code = do
     catch doRep $ \e -> do
@@ -41,9 +41,10 @@ evExpr code = do
               text <- getLine
               let replCode = dsExpr $ parseInput text
               putStrLn $ printExpr replCode
-              putStrLn $ printCps $ toCps replCode 
-              print $ doInfer code replCode
-              putStrLn $ printValue $ doEval code replCode
+              inferT builtinTy replCode
+              --putStrLn $ printCps $ toCps replCode 
+              --print $ doInfer code replCode
+              --putStrLn $ printValue $ doEval code replCode
 
 doEval file expr = eval (ELetRec file expr) builtins
 doInfer file expr = infer builtinTy (ELetRec file expr)
